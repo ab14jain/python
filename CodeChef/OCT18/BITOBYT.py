55f54f8c28e9a0710b3ef7292356a362954bf04e
Abhishek kumar jain <abhishekkumar.jain@outlook.com>
2018-10-17 01:40:08 +0530

    fixed for output

--- CodeChef/OCT18/BITOBYT.py
+++ CodeChef/OCT18/BITOBYT.py
@@ -1,4 +1,5 @@
 import sys
+import math
 
 number_testcase = int(sys.stdin.readline().rstrip())
 inputs = []
@@ -8,14 +9,25 @@ while t:
     inputs.append(int(sys.stdin.readline().rstrip()))
     t -= 1
 
-def get_population(N):
-    time = N
-    bit = 0
-    nibble = 0
-    byte = 0
-    while time:
-
-        time -= 1
-
-for item in inputs:
-    print(get_population(item))
+for n in inputs:
+    _bit = 0
+    _nibble = 0
+    _byte = 0
+    # if n <= 2:           
+    #     _bit = 1
+    # elif n > 2 and n <= 8:
+    #     _nibble = 1
+    # elif n > 8 and n <= 16:
+    #     _byte = 1
+    if(n % 16 > 0 and n % 16 <= 2):
+        _bit = int(math.pow(2, int(n / 16)))
+    elif(n % 16 > 2 and n % 16 <= 8):
+        _nibble = int(math.pow(2, int(n / 16)))
+    elif (n % 16 == 0 or n % 16 > 8):
+        _byte = int(math.pow(2, (int(n / 16) - 1)))
+        # dividend = int(n / 16)
+        # if dividend == 1:
+        #     _byte = int(math.pow(2, (int(n / 16) - 1)))
+        # else:
+        #     _byte = int(math.pow(2, (int(n / 16) - 1)))
+    print( _bit, _nibble, _byte)
